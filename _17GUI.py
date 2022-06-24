98f12184b998d6ecbe8dffd3a75d3a905ffd8823
elenatobi <97405068+elenatobi@users.noreply.github.com>
2022-06-24 22:26:15 +0200

    17 GUI with pygame
    17 GUI with pygame

--- _17GUI.py
+++ _17GUI.py
@@ -1,3 +1,5 @@
+import pygame
+
 CONST_COLORS = {
     "BLACK"      : (  0,   0,   0),
     "WHITE"      : (255, 255, 255),
@@ -46,34 +48,70 @@ class DrawConsole:
     def __init__(self):
         pass
 
-    def drawWidgetBase(self, positions, color):
-        print("WidgetBase: Positions:", positions, "Color:", color)
+    def drawWidgetBase(self, positions, color, border_width):
+        print("WidgetBase: Positions:", positions, "Color:", color, "Border width:", border_width)
 
-    def drawLine(self, positions, color):
-        print("Line: Positions:", positions, "Color:", color)
+    def drawLine(self, positions, color, border_width):
+        print("Line: Positions:", positions, "Color:", color, "Border width:", border_width)
 
-    def drawRect(self, positions, color):
-        print("Rect: Positions:", positions, "Color:", color)
+    def drawRect(self, positions, color, border_width):
+        print("Rect: Positions:", positions, "Color:", color, "Border width:", border_width)
 
-    def drawCircle(self, positions, color):
-        print("Circle: Positions:", positions, "Color:", color)
+    def drawCircle(self, positions, color, border_width):
+        print("Circle: Positions:", positions, "Color:", color, "Border width:", border_width)
 
-    def drawEllipse(self, positions, color):
-        print("Ellipse: Positions:", positions, "Color:", color)
+    def drawEllipse(self, positions, color, border_width):
+        print("Ellipse: Positions:", positions, "Color:", color, "Border width:", border_width)
 
     def drawImage(self, positions, image, source):
         print("Image: Positions:", positions, "Source:", source)
         return image
 
-    def drawLayoutBase(self, positions, color):
-        print("LayoutBase: Positions:", positions, "Color:", color)
+    def drawText(self, positions, text_string, text_surface, color):
+        print("Image: Positions:", positions, "Text:", text_string, "Color:", color)
+        return text_surface
+
+    def drawLayoutBase(self, positions, color, border_width):
+        print("LayoutBase: Positions:", positions, "Color:", color, "Border width:", border_width)
+
+class DrawPygame:
+    def __init__(self, widget):
+        self.widget = widget
+        self.color = get_color("gray")
+        self.running = True
+        self.refresh = True
+        pygame.init()
+        self.screen = pygame.display.set_mode([500, 500])
+
+    def drawRect(self, positions, color, border_width):
+        pygame.draw.rect(self.screen, color, positions, border_width)
+
+    def _handle_sensor(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+    def _draw_screen(self):
+        self.screen.fill(self.color)
+        self.widget.draw(self)
+        pygame.display.flip()
+
+    def run(self):
+        while self.running:
+            self._handle_sensor()
+            if self.refresh:
+                print("PYGAME CONSOLE LOG: REFRESHING CANVAS")
+                self._draw_screen()
+                self.refresh = False
+        pygame.quit()
 
 class WidgetBase:
-    def __init__(self, positions, color_keyword):
+    def __init__(self, positions, color_keyword, border_width = 0):
         if not self.check_valid_position_length(len(positions)):
             raise Exception("Invalid position length of " + type(self).__name__)
         self.positions = positions
         self.color = get_color(color_keyword)
+        self.border_width = border_width
         self.dimensions = 2
 
     def check_valid_position_length(self, position_length):
@@ -93,7 +131,10 @@ class WidgetBase:
 
     def draw(self, draw_object):
         if self.color != CONST_COLORS[COLOR_TRANSPARENCY]:
-            getattr(draw_object, "draw" + type(self).__name__)(self.positions, self.color)
+            getattr(draw_object, "draw" + type(self).__name__)(self.positions, self.color, self.border_width)
+
+    def is_inside(self, x, y):
+        return False
 
 class Line(WidgetBase):
     def check_valid_position_length(self, position_length):
@@ -108,12 +149,19 @@ class Rect(WidgetBase):
     def check_valid_position_length(self, position_length):
         return position_length == 4
 
+    def is_inside(self, x, y):
+        return self.positions[0] < x < self.positions[0] + self.positions[2] and self.positions[1] < y < self.positions[1] + self.positions[3]
+
 class Circle(WidgetBase):
     def check_valid_position_length(self, position_length):
         return position_length == 3
 
+    def is_inside(self, x, y):
+        return (x-self.positions[0])**2 + (y-self.positions[1])**2 < self.positions[2]**2
+
 class Ellipse(Rect):
-    pass
+    def is_inside(self, x, y):
+        return (x-self.positions[0])**2 / self.positions[2]**2 + (y-self.positions[1])**2 / self.positions[3]**2 < 1
 
 class Image(Rect):
     def __init__(self, positions, source):
@@ -124,14 +172,36 @@ class Image(Rect):
     def draw(self, draw_object):
         self.image = getattr(draw_object, "draw" + type(self).__name__)(self.positions, self.image, self.source)
 
-class LayoutBase(Rect):
-    def __init__(self, positions, color_keyword):
+class Text(Rect):
+    def __init__(self, positions, text_string, color_keyword):
         super().__init__(positions, color_keyword)
+        self.text_string = text_string
+        self.font = None
+        self.text_surface = None
+
+    def check_valid_position_length(self, position_length):
+        return position_length == 3
+
+    def draw(self, draw_object):
+        self.text_surface = getattr(draw_object, "draw" + type(self).__name__)(self.positions, self.text_string, self.text_surface, self.color)
+
+class LayoutBase(Rect):
+    def __init__(self, positions, color_keyword, widgets = [], border_width = 0):
+        super().__init__(positions, color_keyword, border_width)
         self.widgets = []
+        if widgets != []:
+            self.append_multiple(widgets)
 
     def append(self, widget):
         widget.move_delta(Vector(self.positions[0], self.positions[1]))
         self.widgets.append(widget)
+
+    def append_multiple(self, widgets):
+        for widget in widgets:
+            self.append(widget)
+
+    def __getitem__(self, index):
+        return self.widgets[index]
 
     def move_delta(self, delta_positions):
         super().move_delta(delta_positions)
@@ -145,9 +215,25 @@ class LayoutBase(Rect):
 
 d = DrawConsole()
 
-l = LayoutBase([5, 8, 100, 100], "green")
-l.append(Line([3, 4, 8, 19], "red"))
-l.append(Rect([4, 5, 2, 3], "lime"))
+"""
+l = LayoutBase([5, 8, 100, 100], "green", [
+    Line([3, 4, 8, 19], "red"),
+    Rect([4, 5, 2, 3], "lime"),
+    Circle([3, 2, 5], "blue"),
+    LayoutBase([3, 20, 97, 80], "cyan", [
+        Ellipse([2, 5, 4, 3], "navy"),
+        Text([12, 3, 5], "Hello world", "black")
+    ]),
+    Rect([23, 2, 10, 5], "red", 3),
+])
 l.move_delta([3, 7])
-print(l)
 l.draw(d)
+"""
+
+#e = Ellipse([3, 6, 2, 3], "blue")
+"""
+e.is_inside(5, 3)
+"""
+
+p = DrawPygame(Rect([10, 10, 50, 50], "green"))
+p.run()
